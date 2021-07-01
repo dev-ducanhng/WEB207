8160e304d5fd3dcc36ebe3aa34799e3109756766
=ducanh6629417 <=ducanh01273635868@gmail.com>
2021-07-01 23:08:50 +0700

    a

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+const myName= <span>Le Trong </span>;
+
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  myName,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
